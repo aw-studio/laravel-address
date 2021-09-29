1e00929ae47430491b3497646e496440c6aa2ca2
Lennart Carstens-Behrens <lennart.carbe@gmail.com>
2021-09-29 11:37:00 +0200

    Update 2021_09_00_000000_create_addresses_table.php

--- migrations/2021_09_00_000000_create_addresses_table.php
+++ migrations/2021_09_00_000000_create_addresses_table.php
@@ -11,7 +11,7 @@ class CreateAddressesTable extends Migration
     public function up()
     {
         Schema::create('addresses', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->morphs('addressable');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
